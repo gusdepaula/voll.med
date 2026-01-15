04754c89cdbf074fd32c8208aedd51ebb6e7765e
Gustavo de Paula <gdepaula@gmail.com>
2026-01-15 11:51:00 -0300

    Nova Migration

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -13,6 +13,7 @@ public record DadosCadastroMedico(
         @NotBlank
         @Email
         String email,
+        @NotBlank
         String telefone,
         @NotBlank @Pattern(regexp = "\\d{4,6}")
         String crm,
