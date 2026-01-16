254b24aebf36a4ab92222c7a7d9670c552ca8737
Gustavo de Paula <gdepaula@gmail.com>
2026-01-16 09:56:57 -0300

    Paginação

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,12 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,9 +26,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(
                 DadosListagemMedico::new
-        ).toList();
+        );
     }
 }
