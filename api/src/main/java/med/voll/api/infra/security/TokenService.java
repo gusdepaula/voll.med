d886f86642cf1014b8da925ee5921637ffcafa7f
Gustavo de Paula <gdepaula@gmail.com>
2026-01-26 14:48:31 -0300

    Outras informações no token

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -23,6 +23,7 @@ public class TokenService {
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
