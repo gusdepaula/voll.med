21bcabaa6652c18b96635d21becec39372edda88
Gustavo de Paula <gdepaula@gmail.com>
2026-01-30 12:05:50 -0300

    Adicionando o agendamento de consultas

---
+++ api/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+        Long id,
+        Long idMedico,
+        @NotNull
+        Long idPaciente,
+        @NotNull
+        @Future
+        LocalDateTime data
+) {
+}
