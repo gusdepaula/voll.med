ba6c8d0b56a0170ee83cbe08949185768eda6ea5
Gustavo de Paula <gdepaula@gmail.com>
2026-02-06 14:30:23 -0300

    Testando código 200

--- api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -1,7 +1,5 @@
 package med.voll.api.domain.consulta;
 
-import org.springframework.cglib.core.Local;
-
 import java.time.LocalDateTime;
 
 public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
--- api/src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ api/src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -51,4 +51,33 @@ class ConsultaControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
+    @Test
+    @DisplayName("Deveria devolver codigo http 200 quando informacoes estao validas")
+    @WithMockUser
+    void agendar_cenario2() throws Exception {
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 2l, 5l, data);
+        when(agendaDeConsultas.agendar(any())).thenReturn(dadosDetalhamento);
+
+        var response = mvc
+                .perform(
+                        post("/consultas")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(dadosAgendamentoConsultaJson.write(
+                                        new DadosAgendamentoConsulta(2l, 5l, data, especialidade)
+                                ).getJson())
+                )
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoConsultaJson.write(
+                dadosDetalhamento
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+
 }
