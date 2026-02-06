403399fd87af25945cce02c37aa835cb889e76af
Gustavo de Paula <gdepaula@gmail.com>
2026-02-06 13:27:24 -0300

    Configurando o banco de testes

---
+++ api/src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,14 @@
+package med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+
+@SpringBootTest
+@AutoConfigureJsonTesters
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+    }
+}
