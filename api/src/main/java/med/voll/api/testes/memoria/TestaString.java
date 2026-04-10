3221e872690320134fd3c817462cf615d400bdbb
Gustavo de Paula <gdepaula@gmail.com>
2026-04-10 11:29:25 -0300

    Concatenando Strings de forma mais eficiente

--- api/src/main/java/med/voll/api/testes/memoria/TestaString.java
+++ api/src/main/java/med/voll/api/testes/memoria/TestaString.java
@@ -3,11 +3,22 @@ package med.voll.api.testes.memoria;
 public class TestaString {
     public static void main(String[] args) {
         String valor1 = "alura";
-        String valor2 = "alura";
+//        String valor2 = "alura";
+//
+//        System.out.println(valor1 == valor2);
+//
+//        String valor3 = new String("alura");
+//        System.out.println(valor1 == valor3);
 
-        System.out.println(valor1 == valor2);
+        valor1 = valor1 + "Programção";
+        System.out.println(valor1);
 
-        String valor3 = new String("alura");
-        System.out.println(valor1 == valor3);
+        String resultado = "";
+        for (int i = 0; i <=100; i++) {
+            resultado = resultado + i + " ";
+        }
+        System.out.println(resultado);
+
+
     }
 }
